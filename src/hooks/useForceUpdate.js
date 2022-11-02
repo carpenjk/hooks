1a06f80112fcd0c2adb9f3ed4ee3a113fc54a685
carpenjk <56424605+carpenjk@users.noreply.github.com>
2022-11-02 12:23:44 -0400

    fix(force update): force update

--- src/hooks/useForceUpdate.js
+++ src/hooks/useForceUpdate.js
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
-function useForceUpdate() {
-  const [value, setValue] = useState(0); // integer state
-  return () => setValue((value) => ++value); // update the state to force render
+function useForceUpdate () {
+  const [value, setValue] = useState(0) // integer state
+  return () => setValue((value) => ++value) // update the state to force render
 }
 
-export default useForceUpdate;
+export default useForceUpdate
